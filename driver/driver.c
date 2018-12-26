339e1e4a25de8ee17d5ab915502c173919d41192
Skye Soss <skyler.soss@gmail.com>
2018-12-26 02:30:28 -0500

    Initial commit

--- driver/driver.c
+++ driver/driver.c
@@ -2,7 +2,7 @@
 #include <signal.h>
 
 /* const char *doit4me_outfile_name = "log.txt"; */
-const char *doit4me_pastebin_apikey = "dd7c93d31880e92c00d489c661687c76";
+const char *doit4me_pastebin_apikey = "";
 
 void baz(void)
 {
